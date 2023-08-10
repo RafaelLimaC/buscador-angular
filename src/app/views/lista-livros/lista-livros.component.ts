67b0fac3b25c9706c958f0611f458d7f2d82e624
Rafael Lima <srafalimacosta@gmail.com>
2023-08-09 22:28:43 -0300

    <fix> usando map

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Subscription, map, switchMap } from 'rxjs';
-import { Item, Livro } from 'src/app/models/interfaces';
+import { filter, map, switchMap, tap } from 'rxjs';
+import { Item } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/services/livro.service';
 
@@ -10,39 +10,25 @@ import { LivroService } from 'src/app/services/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent implements OnDestroy{
+export class ListaLivrosComponent {
 
-  listaLivros: Livro[];
   campoBusca = new FormControl();
-  subscription: Subscription;
-  livro: Livro;
 
   constructor(private service: LivroService) { }
 
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    tap(() => console.log('Fluxo inicial')),
     switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
-    map((items) => this.listaLivros = this.livrosResultadoParaLivros(items))
+    tap(() => console.log('Fluxo inicial')),
+    map((items) => this.livrosResultadoParaLivros(items))
   )
 
-  // buscarLivros() {
-  //   this.subscription = this.service.buscar(this.campoBusca).subscribe({
-  //       next: (items) => {
-  //         this.listaLivros = this.livrosResultadoParaLivros(items)
-  //       },
-  //       error: erro => console.error(erro),
-  //   })
-  // }
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
     return items.map(item => {
       return new LivroVolumeInfo(item)
     })
   }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe()
-  }
-
-
 
 }
